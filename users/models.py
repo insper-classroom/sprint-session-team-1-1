b9481c2ab374568583766a330a955d685fcb3b5e
henriquetg1 <henriquetg1@al.insper.edu.br>
2023-05-19 10:36:09 -0300

    Classe Profile pronta e forms.py feito

--- users/models.py
+++ users/models.py
@@ -1,9 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import User # User é a classe de usuário padrão do Django que já vem com um pacote de autenticação.
 
+
 # Create your models here.
 class Profile(models.Model): # Profile é uma classe que adiciona mais campos/dados ao User do Django, para os usuários bolsistas
-    nome_completo = models.CharField(max_length=100, null=True) # null=True porque quando o usuário se registrar, cria-se automaticamente um objeto do tipo perfil ligado à esse usuário que ainda não preencheu o nome completo. Por isso, inicialmente, no banco de dados, o valor é nulo.
+    
+    Genders = (
+        ('Masculino', 'Masculino'),
+        ('Feminino', 'Feminino'),
+        ('Prefiro não informar', 'Prefiro não informar'),
+        ('Outro', 'Outro'),
+
+    )
+
+    Race = (
+        ('Amarela', 'Amarela'),
+        ('Branca', 'Branca'),
+        ('Indígena', 'Indígena'),
+        ('Parda', 'Parda'),
+        ('Preta', 'Preta'),
+        ('Prefiro não informar', 'Prefiro não informar'),
+        ('Outra', 'Outra'),
+    )
+
+    nome_completo = models.CharField(max_length=150, null=True) # null=True porque quando o usuário se registrar, cria-se automaticamente um objeto do tipo perfil ligado à esse usuário que ainda não preencheu o nome completo. Por isso, inicialmente, no banco de dados, o valor é nulo.
+    email = models.EmailField(max_length=254, null=True) # EmailField é um campo de email, que não pode ser maior que 254 caracteres
+    nome_exibicao = models.CharField(max_length=50, null=True) # Nome que será exibido no site, se não for preenchido, será o nome completo
+    # foto_de_perfil = ...
+    data_nascimento = models.DateField(null=True) # dd/mm/aaaa (vamos usar uma máscara, para as /)
     cpf = models.CharField(max_length=14, null=True) # xxx.xxx.xxx-xx (vamos usar uma máscara, para os . e -)
+    rg = models.CharField(max_length=12, null=True) # xx.xxx.xxx-x (vamos usar uma máscara, para os . e -)
     telefone = models.CharField(max_length=15, null=True) # (xx) xxxxx-xxxx (vamos usar uma máscara, para os () e -)
+    genero = models.CharField(max_length=20, choices=Genders, null=True)
+    outro_genero = models.CharField(max_length=100, blank=True, null=True) # Se o usuário escolher "Outro", ele pode escrever o que quiser aqui
+    cor_ou_raca = models.CharField(max_length=20, choices=Race, null=True)
+    outra_cor_ou_raca = models.CharField(max_length=100, blank=True, null=True)
     usuario = models.OneToOneField(User, on_delete=models.CASCADE) # Um usuário tem UM perfil, e quando o usuário é deletado, o perfil também é (CASCADE).
--- users/views.py
+++ users/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from typing import Any, Dict
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
+from .forms import UserForm 
+from django.urls import reverse_lazy
 
 # Create your views here.
 
 # Aqui nas views, teremos o cadastro de usuários
+class UserCreate(CreateView):
+    # template_name = ""
+    form_class = UserForm # Aqui, definimos o formulário que será usado para o cadastro de usuários
+    # success_url = reverse_lazy('login') # Aqui, definimos a URL para onde o usuário será redirecionado após o cadastro
+
+    def get_context_data(self, *args, **kwargs): # Sobrescrevemos o método get_context_data para adicionar mais campos ao contexto
+        context = super().get_context_data(*args, **kwargs)
+
+        # Aqui, adicionamos os campos do Profile ao contexto para que possam ser preenchidos pelo usuário
+        context['nome_completo'] = self.request.POST.get('nome_completo')
+        context['email'] = self.request.POST.get('email')
+        context['nome_exibicao'] = self.request.POST.get('nome_exibicao')
+        context['data_nascimento'] = self.request.POST.get('data_nascimento')
+        context['cpf'] = self.request.POST.get('cpf')
+        context['rg'] = self.request.POST.get('rg')
+        context['telefone'] = self.request.POST.get('telefone')
+        context['genero'] = self.request.POST.get('genero')
+        context['outro_genero'] = self.request.POST.get('outro_genero')
+        context['cor_ou_raca'] = self.request.POST.get('cor_ou_raca')
+        context['outra_cor_ou_raca'] = self.request.POST.get('outra_cor_ou_raca')
+        
+        return context
