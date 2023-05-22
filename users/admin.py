77b8ae83a3b92d2be45379f99f60fd2540f338b4
henriquetg1 <henriquetg1@al.insper.edu.br>
2023-05-22 13:04:31 -0300

    Profile funciona na criação de users na tela admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin # UserAdmin é a classe de administração padrão para o modelo de usuário do Django
+from .models import User, Profile
 
-# Register your models here.
+class ProfileInline(admin.StackedInline): # admin.StackedInline é uma classe que permite que o Profile seja editado na mesma página do User e que define como um modelo é exibido como um conjunto empilhado de campos em um formulário.
+    model = Profile
+    can_delete = False # Impede que os objetos 'Profile' sejam excluídos em linha junto com o objeto 'User' na página de administração.
+    verbose_name_plural = 'Profile'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline,) # faz com que o formulário 'Profile' seja exibido na página de administração do User
+
+admin.site.unregister(User) # Desregistra o UserAdmin padrão 
+admin.site.register(User, CustomUserAdmin) # Registra o CustomUserAdmin como o novo UserAdmin 
--- users/models.py
+++ users/models.py
@@ -10,7 +10,6 @@ class Profile(models.Model): # Profile é uma classe que adiciona mais campos/da
         ('Feminino', 'Feminino'),
         ('Prefiro não informar', 'Prefiro não informar'),
         ('Outro', 'Outro'),
-
     )
 
     Race = (
@@ -23,16 +22,16 @@ class Profile(models.Model): # Profile é uma classe que adiciona mais campos/da
         ('Outra', 'Outra'),
     )
 
-    nome_completo = models.CharField(max_length=150, null=True) # null=True porque quando o usuário se registrar, cria-se automaticamente um objeto do tipo perfil ligado à esse usuário que ainda não preencheu o nome completo. Por isso, inicialmente, no banco de dados, o valor é nulo.
-    email = models.EmailField(max_length=254, null=True) # EmailField é um campo de email, que não pode ser maior que 254 caracteres
-    nome_exibicao = models.CharField(max_length=50, null=True) # Nome que será exibido no site, se não for preenchido, será o nome completo
+    nome_completo = models.CharField(max_length=150, null=True, verbose_name='Nome Completo') # null=True porque quando o usuário se registrar, cria-se automaticamente um objeto do tipo perfil ligado à esse usuário que ainda não preencheu o nome completo. Por isso, inicialmente, no banco de dados, o valor é nulo.
+    email = models.EmailField(max_length=254, null=True, verbose_name='E-mail') # EmailField é um campo de email, que não pode ser maior que 254 caracteres
+    nome_exibicao = models.CharField(max_length=50, null=True, verbose_name='Nome de exibição') # Nome que será exibido no site, se não for preenchido, será o nome completo
     # foto_de_perfil = ...
-    data_nascimento = models.DateField(null=True) # dd/mm/aaaa (vamos usar uma máscara, para as /)
-    cpf = models.CharField(max_length=14, null=True) # xxx.xxx.xxx-xx (vamos usar uma máscara, para os . e -)
-    rg = models.CharField(max_length=12, null=True) # xx.xxx.xxx-x (vamos usar uma máscara, para os . e -)
+    data_nascimento = models.DateField(null=True, verbose_name='Data de nascimento') # dd/mm/aaaa (vamos usar uma máscara, para as /)
+    cpf = models.CharField(max_length=14, null=True, verbose_name='CPF') # xxx.xxx.xxx-xx (vamos usar uma máscara, para os . e -)
+    rg = models.CharField(max_length=12, null=True, verbose_name='RG') # xx.xxx.xxx-x (vamos usar uma máscara, para os . e -)
     telefone = models.CharField(max_length=15, null=True) # (xx) xxxxx-xxxx (vamos usar uma máscara, para os () e -)
-    genero = models.CharField(max_length=20, choices=Genders, null=True)
-    outro_genero = models.CharField(max_length=100, blank=True, null=True) # Se o usuário escolher "Outro", ele pode escrever o que quiser aqui
-    cor_ou_raca = models.CharField(max_length=20, choices=Race, null=True)
-    outra_cor_ou_raca = models.CharField(max_length=100, blank=True, null=True)
+    genero = models.CharField(max_length=20, choices=Genders, null=True, verbose_name='Gênero') # choices é uma tupla de tuplas, onde o primeiro elemento de cada tupla é o valor que será salvo no banco de dados e o segundo é o valor que será exibido no site (verbose_name é o nome que será exibido no site
+    outro_genero = models.CharField(max_length=100, blank=True, null=True, verbose_name='Outro') # Se o usuário escolher "Outro", ele pode escrever o que quiser aqui
+    cor_ou_raca = models.CharField(max_length=20, choices=Race, null=True, verbose_name='Cor ou Raça')
+    outra_cor_ou_raca = models.CharField(max_length=100, blank=True, null=True, verbose_name='Outra')
     usuario = models.OneToOneField(User, on_delete=models.CASCADE) # Um usuário tem UM perfil, e quando o usuário é deletado, o perfil também é (CASCADE).
