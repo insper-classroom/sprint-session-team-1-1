b9f0da02c343f53662ac910fbfe208f086fbeed1
Luigi Quinze <luigioq@al.insper.edu.br>
2023-05-24 14:57:24 -0300

    url atualizada

--- sprint/urls.py
+++ sprint/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include # include is new
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('accounts/signup/', include('users.urls')),
     path('', include('home.urls')),
-
 ]
