c799008ae4be55ff10ce82fe37956bee818c72a9
Luigi Quinze <luigioq@al.insper.edu.br>
2023-05-24 12:32:23 -0300

    new signup url

--- sprint/urls.py
+++ sprint/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include # include is new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', include('users.urls')),
     path('', include('allauth.urls')),
 ]
