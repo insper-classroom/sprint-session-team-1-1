fb50e1966bcc2c8b17038232dc7468f19e7b7c14
Luigi Quinze <luigioq@al.insper.edu.br>
2023-05-31 12:29:13 -0300

    commit pre merge

--- user_profile/forms.py
+++
@@ -1,8 +0,0 @@
-# from django import forms
-# from django.contrib.auth.models import User
-# from users import models
-# from users import forms
-
-# class UserProfileForm(forms.ModelForm):
-#     model = forms.UserForm
-    
