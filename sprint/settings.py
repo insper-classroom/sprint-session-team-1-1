a0c00c09913f3c58d72ac3acc8a1f3f1764d5d14
Thiago de Miranda e Silva <thimirandasilva@gmail.com>
2023-05-31 13:47:51 -0300

    mudanças css/html

--- sprint/settings.py
+++ sprint/settings.py
@@ -106,7 +106,7 @@ AUTHENTICATION_BACKENDS = [
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
-
+SOCIALACCOUNT_LOGIN_ON_GET = True
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
