defcb02726836e4101e9e07d11823f216ad05e59
Thiago de Miranda e Silva <thimirandasilva@gmail.com>
2023-06-01 19:17:57 -0300

    Mensagens de erro no cadastro feitas

--- sprint/settings.py
+++ sprint/settings.py
@@ -142,7 +142,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us' # linguagem português do Brasil é 'pt-br'
+LANGUAGE_CODE = 'pt-br' # linguagem português do Brasil é 'pt-br'
 
 TIME_ZONE = 'UTC' # fuso horário do Brasil é 'America/Sao_Paulo'
 
