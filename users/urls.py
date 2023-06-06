8875309bf6eda4f3b750f8e606093fe9fec852b1
henriquetg1 <henriquetg1@al.insper.edu.br>
2023-06-06 15:32:04 -0300

    Link aleatório para cadastro

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
+    path('signup/<str:key>', views.signup, name='signup'),
+    path('signup/', views.redirect_home, name='redirect_home'), # redirect_home é uma função que redireciona para a página inicial
     path('profile/edit/', views.edit, name='edit_profile'),
     path('profile/', views.profile, name='profile'),
 ]
