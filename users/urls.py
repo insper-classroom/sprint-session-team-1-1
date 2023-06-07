bc8d4bedf0098fa7de7df7134f818a25e0f612ec
Thiago de Miranda e Silva <thimirandasilva@gmail.com>
2023-06-07 10:39:43 -0300

    Download do PDF

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -10,3 +12,6 @@ urlpatterns = [
     path('profile/edit/', views.edit, name='edit_profile'),
     path('profile/', views.profile, name='profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
